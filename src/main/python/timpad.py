2117be6a61e2f02c1beeb493c2434dd0186ca933
unknown <andrei.cuntan@student.upt.ro>
2022-05-24 19:40:18 +0300

    layout improvements and bug fixes

--- src/main/python/timpad.py
+++ src/main/python/timpad.py
@@ -6,7 +6,7 @@ selected = 0
 bit1 = 0
 bit2 = 0
 # ======= FUNCTIONS ========
-
+# list box stuff
 def onselect(evt):
     w = evt.widget
     try:
@@ -17,7 +17,6 @@ def onselect(evt):
     global selected
     selected = note_id_list[index]
     add_text(note_id_list[index])
-
 
 def sort_list(tup, pos, way):
     list = len(tup)
@@ -64,62 +63,74 @@ def sort_date():
         update_list(sort_list(notelist, 4, 1))
         status.config(text="Sorted last to first")
         bit2 = 1
-def add_note():
-    title = title_box.get()
-    contents = text_box.get("1.0", tk.END)
-    dbops.insert_note(dbops.conn, title, contents)
-    status.config(text="Note added!")
 
-def delete_text():
-    title_box.delete(0, tk.END)
-    text_box.delete('1.0', tk.END)
-
-def save_note():
-    title = title_box.get()
-    contents = text_box.get("1.0", tk.END)
-    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    dbops.update_note(dbops.conn, title, contents, selected, now)
-    status.config(text="Note saved!")
-
+# text box stuff (contents)
 def add_text(idx):
     note = dbops.select_specific_note(dbops.conn, idx)
     title_box.insert(0, note[0])
     text_box.insert('1.0', note[1])
-
+def delete_text():
+    title_box.delete(0, tk.END)
+    text_box.delete('1.0', tk.END)
+def add_note():
+    title = title_box.get()
+    contents = text_box.get("1.0", tk.END)
+    print("a"+title+"a")
+    print("b" + contents + "b")
+    if title != "":
+        dbops.insert_note(dbops.conn, title, contents)
+        update_list(dbops.select_all_notes(dbops.conn))
+        status.config(text="Note added!")
+    else:
+        status.config(text="Note doesn't have a title")
+def save_note():
+    if selected:
+        title = title_box.get()
+        contents = text_box.get("1.0", tk.END)
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        dbops.update_note(dbops.conn, title, contents, selected, now)
+        status.config(text="Note saved!")
+    else:
+        status.config(text="Note was not selected!")
 def delete_note():
-    delete_text()
-    dbops.delete_note(dbops.conn, selected)
-    update_list(dbops.select_all_notes(dbops.conn))
-    status.config(text="Note deleted!")
+    global selected
+    if selected:
+        delete_text()
+        dbops.delete_note(dbops.conn, selected)
+        update_list(dbops.select_all_notes(dbops.conn))
+        status.config(text="Note deleted!")
+    else:
+        status.config(text="Note was not selected!")
+
 # ======= USER INTERFACE ========
 window = tk.Tk()
 list_frame = tk.Frame()
+list_frame.pack(side=tk.LEFT)
 content_frame = tk.Frame()
-greeting = tk.Label(text="Hello, Tkinter")
-title_box = tk.Entry(master=content_frame)
-text_box = tk.Text(master=content_frame)
-name_btn = tk.Button(master=list_frame, text="Name", command=sort_name)
-date_btn = tk.Button(master=list_frame, text="Date", command=sort_date)
-list_box = tk.Listbox(master=list_frame, width=70)
-status = tk.Label(master=list_frame, text="")
+content_frame.pack(side=tk.RIGHT)
+greeting = tk.Label(master=list_frame, text="Hello, Tkinter")
 greeting.pack()
+title_box = tk.Entry(master=content_frame, width=100)
 title_box.pack()
+text_box = tk.Text(master=content_frame, width=75)
 text_box.pack()
-name_btn.pack()
-date_btn.pack()
-list_box.pack()
+status = tk.Label(master=list_frame, text="")
 status.pack()
+list_box = tk.Listbox(master=list_frame, width=70, height=20)
+list_box.pack()
 list_box.bind('<<ListboxSelect>>', onselect)
 update_list(dbops.select_all_notes(dbops.conn))
 # buttons
-add = tk.Button(master=list_frame, text="Add", command=add_note)
-edit = tk.Button(master=list_frame, text="Save", command=save_note)
-delete = tk.Button(master=list_frame, text="Delete", command=delete_note)
-add.pack()
-edit.pack()
-delete.pack()
-list_frame.pack(side=tk.LEFT)
-content_frame.pack(side=tk.RIGHT)
+name_btn = tk.Button(master=list_frame, text="Sort name", width=8, command=sort_name)
+name_btn.pack(side=tk.LEFT)
+date_btn = tk.Button(master=list_frame, text="Sort date", width=8, command=sort_date)
+date_btn.pack(side=tk.LEFT)
+add = tk.Button(master=list_frame, text="Add", width=8, command=add_note)
+add.pack(side=tk.LEFT)
+edit = tk.Button(master=list_frame, text="Save", width=8, command=save_note)
+edit.pack(side=tk.LEFT)
+delete = tk.Button(master=list_frame, text="Delete", width=8, command=delete_note)
+delete.pack(side=tk.LEFT)
 window.mainloop()
 def helloworld(out):
     out.write("Hello world of Python\n")
